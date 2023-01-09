2fb13b62f44fac4a8183cdb3219ed7f10de34e71
varunsub <varun.subramanyan@gmail.com>
2023-01-09 15:23:29 -0800

    Added password fields

--- front/necx/src/Components/SignUpTextfield.js
+++ front/necx/src/Components/SignUpTextfield.js
@@ -1,12 +1,20 @@
-import { TextField, Input } from "@mui/material";
+import { TextField } from "@mui/material";
 import { Controller } from "react-hook-form";
+import "../styles/main.css";
 export default function SignUpTextfield({ name, placeholder, control }) {
   return (
     <Controller
       name={name}
+      className="login-input"
       control={control}
       render={({ field: { onChange, value } }) => (
-        <TextField onChange={onChange} value={value} label={placeholder} />
+        <TextField
+          className="login-input"
+          onChange={onChange}
+          value={value}
+          label={placeholder}
+          type={name == "password" || name == "cpassword" ? "password" : null}
+        />
       )}
     />
   );
